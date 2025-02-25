09eca9ef849f62f8595da246a0277dc26558ef02
Ashley Sanders <90638319+ashleysand@users.noreply.github.com>
2025-02-25 12:17:20 -0500

    updating the port # inside of the server.js

--- registration-backend/server.js
+++ registration-backend/server.js
@@ -54,5 +54,5 @@ app.post("/register", async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
